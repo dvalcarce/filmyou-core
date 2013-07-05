ebd3e1ec88345d7e7115e2d7b6cd8881e0a3a021
Daniel Valcarce Silva <danivalsil@gmail.com>
2013-07-05 14:41:24 +0200

    Project skeleton.

---
+++ src/main/java/es/udc/fi/dc/irlab/baselinerecommender/App.java
@@ -0,0 +1,12 @@
+package es.udc.fi.dc.irlab.baselinerecommender;
+
+import org.apache.mahout.cf.taste.hadoop.item.RecommenderJob;
+
+public class App {
+
+	public static void main(String args[]) {
+		RecommenderJob j = new RecommenderJob();
+		System.out.print("Hello world!");
+	}
+
+}
